782092c5547b7107d40a6705da50695a1941f35d
Leonardo Almeida <leonardograselalmeida@gmail.com>
2023-02-06 16:54:02 -0300

    estrategia 1

--- RestaurantDomainTests/RemoteRestaurantLoaderTests.swift
+++ RestaurantDomainTests/RemoteRestaurantLoaderTests.swift
@@ -34,31 +34,51 @@ final class RemoteRestaurantLoaderTests: XCTestCase {
     func test_load_and_returned_error_for_connectivity() throws {
         let anyURL = try XCTUnwrap(URL(string: "http://comitando.com.br"))
         let client = NetworkClientSpy()
-        let sut = RemoteRestaurantLoader(url: anyURL, networkClient: client )
+        client.stateHandler = .error(client.anyError())
+        let sut = RemoteRestaurantLoader(url: anyURL, networkClient: client)
         
         let exp = expectation(description: "esperando retorno da clousure")
-        var returnedError: Error?
-        sut.load { error in
-            returnedError = error
+        var returnedResult: RemoteRestaurantLoader.Error?
+        sut.load { result in
+            returnedResult = result
             exp.fulfill()
         }
         
         wait(for: [exp], timeout: 1.0)
         
-        XCTAssertNotNil(returnedError)
+        XCTAssertEqual(returnedResult, .connectivity)
+    }
+    
+    func test_load_and_returned_error_for_invalidData() throws {
+        let anyURL = try XCTUnwrap(URL(string: "http://comitando.com.br"))
+        let client = NetworkClientSpy()
+        client.stateHandler = .success
+        let sut = RemoteRestaurantLoader(url: anyURL, networkClient: client )
+        
+        let exp = expectation(description: "esperando retorno da clousure")
+        var returnedResult: RemoteRestaurantLoader.Error?
+        sut.load { result in
+            returnedResult = result
+            exp.fulfill()
+        }
+        
+        wait(for: [exp], timeout: 1.0)
+        
+        XCTAssertEqual(returnedResult, .invalidData)
     }
 }
 
 final class NetworkClientSpy: NetworkClient {
     
     private(set) var urlRequests: [URL?] = []
+    var stateHandler: NetworkState?
     
-    func request(from url: URL, completion: @escaping (Error) -> Void) {
+    func request(from url: URL, completion: @escaping (NetworkState) -> Void) {
         urlRequests.append(url)
-        completion(anyError())
+        completion(stateHandler ?? .error(anyError()))
     }
     
-    private func anyError() -> Error {
+    fileprivate func anyError() -> Error {
         return NSError(domain: "any error", code: 1)
     }
 }
