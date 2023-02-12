eebcf7dd3877000e3d29e4f96b12c6b13bfba951
Leonardo Almeida <leonardograselalmeida@gmail.com>
2023-02-12 16:40:29 -0300

    criando cenário de erro para nosso NetworkService

--- RestaurantDomain/NetworkClient/NetworkClient.swift
+++ RestaurantDomain/NetworkClient/NetworkClient.swift
@@ -11,3 +11,25 @@ public protocol NetworkClient {
     typealias NetworkResult = Result<(Data, HTTPURLResponse), Error>
     func request(from url: URL, completion: @escaping (NetworkResult) -> Void)
 }
+
+final class NetworkService: NetworkClient {
+    let session: URLSession
+    
+    init(session: URLSession) {
+        self.session = session
+    }
+    
+    func request(from url: URL, completion: @escaping (NetworkResult) -> Void) {
+        session.dataTask(with: url) { data, response, error in
+            if let error {
+                completion(.failure(error))
+            } else if let data, let response = response as? HTTPURLResponse {
+                completion(.success((data, response)))
+            } else {
+                let error = NSError(domain: "unexpected values", code: -1)
+                completion(.failure(error))
+            }
+        }.resume()
+    }
+    
+}
---
+++ RestaurantDomainTests/NetworkClient/NetworkServiceTests.swift
@@ -0,0 +1,89 @@
+//
+//  NetworkServiceTests.swift
+//  RestaurantDomainTests
+//
+//  Created by Leonardo Almeida on 12/02/23.
+//
+
+import XCTest
+@testable import RestaurantDomain
+
+final class NetworkServiceTests: XCTestCase {
+    
+    func test_loadRequest_resume_dataTask_with_url() {
+        let url = URL(string: "https://comitando.com.br")!
+        let session = URLSessionSpy()
+        let task = URLSessionDataTaskSpy()
+        let sut = NetworkService(session: session)
+        
+        session.stub(url: url, task: task)
+        
+        sut.request(from: url) { _ in }
+        
+        XCTAssertEqual(task.resumeCount, 1)
+    }
+    
+    func test_loadRequest_and_completion_with_error() {
+        let url = URL(string: "https://comitando.com.br")!
+        let session = URLSessionSpy()
+        let task = URLSessionDataTaskSpy()
+        let sut = NetworkService(session: session)
+        
+        let anyError = NSError(domain: "any error", code: -1)
+        session.stub(url: url, task: task, error: anyError)
+        
+        let exp = expectation(description: "aguardando retorno da closure")
+        sut.request(from: url) { result in
+            
+            switch result {
+            case let .failure(returnedError):
+                XCTAssertEqual(returnedError as NSError, anyError)
+            default:
+                XCTFail("Esperado falha, porem retornou \(result)")
+            }
+            
+            exp.fulfill()
+        }
+        
+        wait(for: [exp], timeout: 1.0) 
+    }
+}
+
+
+final class URLSessionSpy: URLSession {
+    
+    private(set) var stubs: [URL: Stub] = [:]
+    
+    struct Stub {
+        let task: URLSessionDataTask
+        let error: Error?
+    }
+    
+    func stub(url: URL, task: URLSessionDataTask, error: Error? = nil) {
+        stubs[url] = Stub(task: task, error: error)
+    }
+    
+    override func dataTask(with url: URL, completionHandler: @escaping (Data?, URLResponse?, Error?) -> Void) -> URLSessionDataTask {
+        guard let stub = stubs[url] else {
+            return FakeURLSessionDataTask()
+        }
+        
+        completionHandler(nil, nil, stub.error)
+        
+        return stub.task
+    }
+    
+}
+
+final class URLSessionDataTaskSpy: URLSessionDataTask {
+    private(set) var resumeCount = 0
+    
+    override func resume() {
+        resumeCount += 1
+    }
+}
+
+final class FakeURLSessionDataTask: URLSessionDataTask {
+    override func resume() {
+    }
+}
