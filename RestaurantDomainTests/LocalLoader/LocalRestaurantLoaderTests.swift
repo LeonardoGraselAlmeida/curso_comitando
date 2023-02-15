8aa4e31469a2bca21a78504ab1e7b43dfd6d1aa8
Leonardo Almeida <leonardograselalmeida@gmail.com>
2023-02-15 05:31:44 -0300

    tratando casos de erro, o sistema envia erro quando a ação deletar não funciona e o sistema recebe erro quando a ação de salvar não funciona

--- RestaurantDomainTests/LocalLoader/LocalRestaurantLoaderTests.swift
+++ RestaurantDomainTests/LocalLoader/LocalRestaurantLoaderTests.swift
@@ -12,7 +12,7 @@ final class LocalRestaurantLoaderTests: XCTestCase {
     
     func test_save_deletes_old_cache() {
         let (sut, cache) = makeSUT()
-        let items: [RestaurantItem] = [RestaurantItem(id: UUID(), name: "name", location: "location", distance: 5.5, ratings: 0, parasols: 0)]
+        let items: [RestaurantItem] = [makeItem()]
         
         sut.save(items) {_ in }
         
@@ -22,12 +22,43 @@ final class LocalRestaurantLoaderTests: XCTestCase {
     func test_save_insert_new_data_on_cache() {
         let currentDate = Date()
         let (sut, cache) = makeSUT(currentDate: currentDate)
-        let items: [RestaurantItem] = [RestaurantItem(id: UUID(), name: "name", location: "location", distance: 5.5, ratings: 0, parasols: 0)]
+        let items: [RestaurantItem] = [makeItem()]
         
         sut.save(items) {_ in }
         cache.completionHandlerForDelete(nil)
         
         XCTAssertEqual(cache.methodsCalled, [.delete, .save(items: items, timestamp: currentDate)])
+    }
+    
+    func test_save_fails_after_delete_old_cache() {
+        let (sut, cache) = makeSUT()
+        let items: [RestaurantItem] = [makeItem()]
+        
+        var returnedError: Error?
+        sut.save(items) { error in
+            returnedError = error
+        }
+        
+        let anyError = NSError(domain: "any error", code: -1)
+        cache.completionHandlerForDelete(anyError)
+        
+        XCTAssertEqual(returnedError as? NSError, anyError)
+    }
+    
+    func test_save_fails_after_insert_new_data_cache() {
+        let (sut, cache) = makeSUT()
+        let items: [RestaurantItem] = [makeItem()]
+        
+        var returnedError: Error?
+        sut.save(items) { error in
+            returnedError = error
+        }
+        
+        let anyError = NSError(domain: "any error", code: -1)
+        cache.completionHandlerForDelete(nil)
+        cache.completionHandlerForInsert(anyError)
+        
+        XCTAssertEqual(returnedError as? NSError, anyError)
     }
     
     private func makeSUT(currentDate: Date = Date(), file: StaticString = #filePath, line: UInt = #line) -> (sut: LocalRestaurantLoader, cache: CacheClientSpy) {
@@ -39,6 +70,10 @@ final class LocalRestaurantLoaderTests: XCTestCase {
         trackForMemoryLeaks(sut)
         
         return (sut, cache)
+    }
+    
+    private func makeItem() -> RestaurantItem {
+        return RestaurantItem(id: UUID(), name: "name", location: "location", distance: 5.5, ratings: 0, parasols: 0)
     }
 }
 
@@ -62,6 +97,10 @@ final class CacheClientSpy: CacheClient {
     }
     
     func completionHandlerForDelete(_ error: Error?) {
+        completionHandler?(error)
+    }
+    
+    func completionHandlerForInsert(_ error: Error?) {
         completionHandler?(error)
     }
 }
