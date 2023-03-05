cf9ee3eb7cdb685a7bca563e3870e4fe7689e1b9
Leonardo Almeida <leonardograselalmeida@gmail.com>
2023-03-05 13:20:29 -0300

    separando arquivos de cache client

---
+++ RestaurantDomain/LocalLoader/Cache/CacheClient.swift
@@ -0,0 +1,24 @@
+//
+//  CacheClient.swift
+//  RestaurantDomain
+//
+//  Created by Leonardo Almeida on 05/03/23.
+//
+
+import Foundation
+
+public enum LoadResultState {
+    case empty
+    case success(items: [RestaurantItem], timestamp: Date)
+    case failure(Error)
+}
+
+public protocol CacheClient {
+    typealias SaveResult = (Error?) -> Void
+    typealias DeleteResult = (Error?) -> Void
+    typealias LoadResult = (LoadResultState) -> Void
+    
+    func save(_ items: [RestaurantItem], timestamp: Date, completion: @escaping SaveResult)
+    func delete(completion: @escaping DeleteResult)
+    func load(completion: @escaping LoadResult)
+}
--- RestaurantDomain/LocalLoader/LocalRestaurantLoader.swift
+++ RestaurantDomain/LocalLoader/LocalRestaurantLoader.swift
@@ -7,22 +7,6 @@
 
 import Foundation
 
-public enum LoadResultState {
-    case empty
-    case success(items: [RestaurantItem], timestamp: Date)
-    case failure(Error)
-}
-
-public protocol CacheClient {
-    typealias SaveResult = (Error?) -> Void
-    typealias DeleteResult = (Error?) -> Void
-    typealias LoadResult = (LoadResultState) -> Void
-    
-    func save(_ items: [RestaurantItem], timestamp: Date, completion: @escaping SaveResult)
-    func delete(completion: @escaping DeleteResult)
-    func load(completion: @escaping LoadResult)
-}
-
 public final class LocalRestaurantLoader {
     
     private let cache: CacheClient
